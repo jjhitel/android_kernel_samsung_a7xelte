f22d1c7ced29a75b7c1b2f9f2effa61a6f1dbaae
Jeong Seong-pil <jjjhitel@gmail.com>
2017-03-27 02:18:43 +0900

    Fix compile errors

--- kernel/fork.c
+++ kernel/fork.c
@@ -1549,8 +1549,6 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 			attach_pid(p, PIDTYPE_SID, task_session(current));
 			list_add_tail(&p->sibling, &p->real_parent->children);
 			list_add_tail_rcu(&p->tasks, &init_task.tasks);
-			attach_pid(p, PIDTYPE_PGID);
-			attach_pid(p, PIDTYPE_SID);
 #ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
 			add_2_adj_tree(p);
 #endif
